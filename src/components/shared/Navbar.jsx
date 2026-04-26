fa34fc81d4ff22f6a6f7977ade8cebc1db6f114c
AforArdi <binsbro36@gmail.com>
2026-04-26 23:00:56 +0600

    working on adding navbar login logout button and showing user info like name and image

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,9 +1,16 @@
+'use client'
+
 import Image from "next/image";
 import Link from "next/link";
 import userIcon from '@/assets/user.png'
 import NavLink from "./NavLink";
+import { authClient } from "@/lib/auth-client";
 
 const Navbar = () => {
+    const { data: session } = authClient.useSession();
+    const user = session?.user;
+    console.log(user.image);
+
     return ( 
         <div className="container mx-auto flex items-center justify-between mt-7">
             <div></div>
@@ -13,7 +20,7 @@ const Navbar = () => {
                 <ul><NavLink href={'/career'}>Career</NavLink></ul>
             </div>
             <div className="flex items-center gap-3">
-                <Image src={userIcon} alt="User Icon" width={40} height={40}></Image>
+                <Image src={user.image || userIcon} alt="User Icon" width={40} height={40}></Image>
                 <Link href={'/login'}>
                     <button className="btn bg-[#403F3F] text-white">Login</button>
                 </Link>
